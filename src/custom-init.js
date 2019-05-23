466a3d53f48e5ce5e89b977b88eaabcf1d2b3a71
James Stephenson <james.stephenson@appian.com>
2019-05-23 12:51:58 -0400

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Added customizations and build file for asar

--- src/custom-init.js
+++ src/custom-init.js
@@ -1,7 +1,5 @@
 const {remote} = require('electron')
 
-const directMessagesHeader = '<content role="listitem" data-group-id="dm" class="dm-header" style="order: 1;"><div role="heading" aria-level="2" class="k7aBq"><div class="aOHsTc wJNchb" jsname="dms">Direct Messages</div></div></content>';
-
 document.addEventListener("keydown", function(e) {
   if (e.which === 123) {
     remote.getCurrentWindow().openDevTools();
@@ -42,84 +40,4 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 
-  watchMainPane();
 });
-
-function watchMainPane() {
-  var box = document.querySelector("div#yDmH0d");
-  var config = { childList: true };
-
-  var mainPane = box.querySelector(":scope > c-wiz:not(.oCHqfe)");
-  var roomObserver = initializeSortedRoomList(mainPane);
-
-  var callback = function(mutationsList, observer) {
-    mutationsList.forEach((mutation) => {
-      switch(mutation.type) {
-        case 'childList':
-          var mainPane = mutation.target.querySelector(":scope > c-wiz:not(.oCHqfe)");
-          if (mainPane != null) {
-            if (roomObserver != null) {
-              roomObserver.disconnect();
-              roomObserver = null;
-            }
-
-            roomObserver = initializeSortedRoomList(mainPane);
-          }
-
-          break;
-      }
-    });
-  }
-
-  var observer = new MutationObserver(callback);
-  observer.observe(box, config);
-
-  return observer;
-}
-
-function initializeSortedRoomList(mainPane) {
-  var recentRooms = mainPane.querySelector("div.vHL80e > div[role='list']");
-
-  // Insert the "Direct Messages" header if it does not exist
-  if (recentRooms.querySelector("content.dm-header") == null) {
-    var template = document.createElement('template');
-    template.innerHTML = directMessagesHeader;
-    recentRooms.appendChild(template.content.firstChild);
-  }
-
-  organizeRooms(recentRooms);
-
-  var observer = new MutationObserver(
-    (mutationsList, observer) => {
-      mutationsList.forEach((mutation) => {
-        switch(mutation.type) {
-          case 'childList':
-            organizeRooms(recentRooms);
-            break;
-        }
-      });
-    }
-  );
-
-  var config = { childList: true, subtree: true }
-  observer.observe(recentRooms, config);
-
-  return observer;
-}
-
-function partition(array, isValid) {
-  return array.reduce(([pass, fail], elem) => {
-    return isValid(elem) ? [[...pass, elem], fail] : [pass, [...fail, elem]];
-  }, [[], []]);
-}
-
-function organizeRooms(parentNode) {
-  var items = Array.from(parentNode.querySelectorAll("content.dHI9xe"));
-  var [roomItems, dmItems] = partition(items, (room) => {
-    return room.querySelector(".t5F5nf > .w3vN9b") == null && !room.getAttribute("data-group-id").startsWith("newdm");
-  });
-
-  roomItems.forEach (e => { e.style.order = 0; });
-  dmItems.forEach (e => { e.style.order = 2; });
-}
-
